3fc889d5b576e19248be7f5a6ce66167985d5fb7
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-10-04 15:28:56 +0300

    finish

--- 2709prac/MainWindow.xaml.cs
+++ 2709prac/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,10 +21,29 @@ namespace _2709prac
     /// </summary>
     public partial class MainWindow : Window
     {
+        Popup[] popups;
         public MainWindow()
         {
             InitializeComponent();
-            
+            popups = new Popup[] { PP1, PP2 };
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < popups.Length; i++)
+            {
+                popups[i].IsOpen = false;
+            }
+        }
+
+        private void OnMouseEnterText(object sender, MouseEventArgs e)
+        {
+            PP1.IsOpen = true;
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PP2.IsOpen = true;
         }
     }
 }
